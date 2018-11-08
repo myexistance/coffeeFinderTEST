01728116e4a383ab0cdba481cfe83fbf29701127
Hojoong Yoon <932400976@bun2-206-15.local>
2018-11-08 15:36:07 +1300

    jquery added

---
+++ js/filterCoffeeFinder.js
@@ -0,0 +1,76 @@
+// $(function(){
+//   $('#all').click(function(){
+//     $('.item').show();
+//     return false;
+//   });
+  
+//   $('#cbd').click(function(){
+//     $('.item').show();
+    function allCbd(){
+      $(function(){
+        $(".cbd").show();
+      });
+
+      $(function(){
+        $(".teAro").hide();
+      });
+
+      $(function(){
+        $(".newtown").hide();
+      });
+
+      $(function(){
+        $(".dashboard").hide();
+      });
+    }
+
+//     $('.item').not('.cbd').hide();
+//     return false;
+//   });
+  
+//   $('#TeAro').click(function(){
+//     $('.item').show();
+//     $('.item').not('.TeAro').hide();
+//     return false;
+//   });
+  
+//   $('#newTown').click(function(){
+//     $('.item').show();
+//     $('.item').not('.newTown').hide();
+//     return false;
+//   });
+//   $('#FlatWhite').click(function(){
+//     $('.item').show();
+//     $('.item').not('.FlatWhite').hide();
+//     return false;
+//   });
+  
+//   $('#LongB').click(function(){
+//     $('.item').show();
+//     $('.item').not('.LongB').hide();
+//     return false;
+//   });
+  
+//   $('#Cappucino').click(function(){
+//     $('.item').show();
+//     $('.item').not('.Cappucino').hide();
+//     return false;
+//   });
+  
+//   $('#HAVANA').click(function(){
+//     $('.item').show();
+//     $('.item').not('.HAVANA').hide();
+//     return false;
+//   });
+//   $('#FLIGHT').click(function(){
+//     $('.item').show();
+//     $('.item').not('.FLIGHT').hide();
+//     return false;
+//   });
+  
+//   $('#MOJO').click(function(){
+//     $('.item').show();
+//     $('.item').not('.MOJO').hide();
+//     return false;
+//   });
+// });
