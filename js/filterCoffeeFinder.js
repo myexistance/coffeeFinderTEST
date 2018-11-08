93abd31b5f92bb19f1aec73a356bfc6e731c2140
myexistance <joonyoonlives@gmail.com>
2018-11-08 16:58:21 +1300

    jquery added

--- js/filterCoffeeFinder.js
+++ js/filterCoffeeFinder.js
@@ -24,53 +24,38 @@
       });
     }
 
-//     $('.item').not('.cbd').hide();
-//     return false;
-//   });
-  
-//   $('#TeAro').click(function(){
-//     $('.item').show();
-//     $('.item').not('.TeAro').hide();
-//     return false;
-//   });
-  
-//   $('#newTown').click(function(){
-//     $('.item').show();
-//     $('.item').not('.newTown').hide();
-//     return false;
-//   });
-//   $('#FlatWhite').click(function(){
-//     $('.item').show();
-//     $('.item').not('.FlatWhite').hide();
-//     return false;
-//   });
-  
-//   $('#LongB').click(function(){
-//     $('.item').show();
-//     $('.item').not('.LongB').hide();
-//     return false;
-//   });
-  
-//   $('#Cappucino').click(function(){
-//     $('.item').show();
-//     $('.item').not('.Cappucino').hide();
-//     return false;
-//   });
-  
-//   $('#HAVANA').click(function(){
-//     $('.item').show();
-//     $('.item').not('.HAVANA').hide();
-//     return false;
-//   });
-//   $('#FLIGHT').click(function(){
-//     $('.item').show();
-//     $('.item').not('.FLIGHT').hide();
-//     return false;
-//   });
-  
-//   $('#MOJO').click(function(){
-//     $('.item').show();
-//     $('.item').not('.MOJO').hide();
-//     return false;
-//   });
-// });
+    function allteAro(){
+      $(function(){
+        $(".teAro").show();
+      });
+
+      $(function(){
+        $(".cbd").hide();
+      });
+
+      $(function(){
+        $(".newtown").hide();
+      });
+
+      $(function(){
+        $(".dashboard").hide();
+      });
+    }
+    function allNewTown(){
+      $(function(){
+        $(".newtown").show();
+      });
+
+      $(function(){
+        $(".cbd").hide();
+      });
+
+      $(function(){
+        $(".teAro").hide();
+      });
+
+      $(function(){
+        $(".dashboard").hide();
+      });
+    }
+
